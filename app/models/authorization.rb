b990304b192fc6e3ab509ac7bd01198835dbf111
duende84 <andreselduende@gmail.com>
2013-04-05 20:31:17 -0500

    change brand in navbar to logo image - add random password to facebook register user

--- app/models/authorization.rb
+++ app/models/authorization.rb
@@ -20,14 +20,15 @@ class Authorization < ActiveRecord::Base
 	def self.find_or_create(auth_hash)
 	  unless auth = find_by_provider_and_uid(auth_hash["provider"], auth_hash["uid"])
 	    unless user = User.find_by_email(auth_hash["info"]["email"])
+	    	rand_password=('0'..'z').to_a.shuffle.first(8).join
 	    	user = User.create :name => auth_hash["info"]["name"],
 		                       :email => auth_hash["info"]["email"],
 		                       :nickname => auth_hash["info"]["nickname"],
 		                       :birthday => format_facebook_date(auth_hash["extra"]["raw_info"]["birthday"]),
 		                       :gender => auth_hash["extra"]["raw_info"]["gender"],
 		                       :user_type => UserType.find_by_name("facebook"),
-		                       :password => '1234567',
-		                       :password_confirmation => '1234567'
+		                       :password => rand_password,
+		                       :password_confirmation => rand_password
 	    end
 		    auth = create :user => user, :provider => auth_hash["provider"], :uid => auth_hash["uid"]
 	  end
