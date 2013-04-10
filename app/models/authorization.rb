c4380f1312724e4c1447f346ad6b463867f0c9b0
duende84 <andreselduende@gmail.com>
2013-04-09 23:26:07 -0500

    update random pass generator and welcome email for facebook users

--- app/models/authorization.rb
+++ app/models/authorization.rb
@@ -20,15 +20,14 @@ class Authorization < ActiveRecord::Base
 	def self.find_or_create(auth_hash)
 	  unless auth = find_by_provider_and_uid(auth_hash["provider"], auth_hash["uid"])
 	    unless user = User.find_by_email(auth_hash["info"]["email"])
-	    	rand_password=('0'..'z').to_a.shuffle.first(8).join
 	    	user = User.create :name => auth_hash["info"]["name"],
 		                       :email => auth_hash["info"]["email"],
 		                       :nickname => auth_hash["info"]["nickname"],
 		                       :birthday => format_facebook_date(auth_hash["extra"]["raw_info"]["birthday"]),
 		                       :gender => auth_hash["extra"]["raw_info"]["gender"],
 		                       :user_type => UserType.find_by_name("facebook"),
-		                       :password => rand_password,
-		                       :password_confirmation => rand_password
+		                       :password => auth_hash["uid"],
+		                       :password_confirmation => auth_hash["uid"]
 	    end
 		    auth = create :user => user, :provider => auth_hash["provider"], :uid => auth_hash["uid"]
 	  end
--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -5,7 +5,9 @@ Rails.application.config.middleware.use OmniAuth::Builder do
 		provider :facebook, '500414270016500', '882c5e95753ef6ec7f216969b4d405ce',
 						 :scope => 'email,user_birthday'
 	else
-		provider :facebook, '584950538198110', 'd12da22d298e172a824a65da7c27ef06',
-  				   :scope => 'email,user_birthday'
+	  #provider :facebook, '584950538198110', 'd12da22d298e172a824a65da7c27ef06',
+   	#			   :scope => 'email,user_birthday'
+  	provider :facebook, '313638042099221', '0d2cd821cd2e10ea3f266b56bd407018',
+  					 :scope => 'email,user_birthday'
 	end
 end
